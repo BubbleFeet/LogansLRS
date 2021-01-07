75c39a870f6fb5638eb672268b573d9a62aa099d
Michael Logan <michael.logan.duxbury@gmail.com>
2021-01-07 15:19:51 -0800

    -Fixed home page navbar name bug -started shippingscreen implementation

---
+++ frontend/src/screens/ShippingScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import React, { useState } from 'react'
+import { Form, Button } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import FormContainer from '../components/FormContainer'
+
+const ShippingScreen = ({ history }) => {
+	return <div>Shipping</div>
+}
+
+export default ShippingScreen
