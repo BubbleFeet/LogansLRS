4ab663a232943b819b485016f93b93cd40ef7e48
Michael Logan <michael.logan.duxbury@gmail.com>
2021-01-21 15:05:04 -0800

    -Search functionality now implemented

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -6,7 +6,9 @@ import Message from '../components/Message'
 import Loader from '../components/Loader'
 import { listProducts } from '../actions/productActions'
 
-const HomeScreen = () => {
+const HomeScreen = ({ match }) => {
+	const keyword = match.params.keyword
+
 	const dispatch = useDispatch()
 
 	const productList = useSelector((state) => state.productList)
@@ -14,9 +16,9 @@ const HomeScreen = () => {
 
 	useEffect(
 		() => {
-			dispatch(listProducts())
+			dispatch(listProducts(keyword))
 		},
-		[ dispatch ]
+		[ dispatch, keyword ]
 	)
 
 	return (
