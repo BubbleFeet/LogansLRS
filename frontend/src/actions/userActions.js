a6ee77c5fce34ae2b09f6043299fbe2aeb7f71c7
Michael Logan <michael.logan.duxbury@gmail.com>
2021-01-21 12:35:00 -0800

    -Frontend product review func implemented -Local storage info should now clear on logout

--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -26,6 +26,7 @@ import {
 	USER_UPDATE_SUCCESS
 } from '../constants/userConstants'
 import { ORDER_LIST_MY_RESET } from '../constants/orderConstants'
+import { PRODUCT_CREATE_REVIEW_RESET } from '../constants/productConstants'
 
 export const login = (email, password) => async (dispatch) => {
 	try {
@@ -57,10 +58,15 @@ export const login = (email, password) => async (dispatch) => {
 
 export const logout = () => (dispatch) => {
 	localStorage.removeItem('userInfo')
+	localStorage.removeItem('cartItems')
+	localStorage.removeItem('shippingAddress')
+	localStorage.removeItem('paymentMethod')
 	dispatch({ type: USER_LOGOUT })
 	dispatch({ type: USER_DETAILS_RESET })
 	dispatch({ type: ORDER_LIST_MY_RESET })
 	dispatch({ type: USER_LIST_RESET })
+	dispatch({ type: PRODUCT_CREATE_REVIEW_RESET })
+	document.location.href = '/login'
 }
 
 export const register = (name, email, password) => async (dispatch) => {
